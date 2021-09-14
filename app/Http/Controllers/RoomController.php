7b174c457b5ff619d1403965058123d229b2f094
yongho0720 <roh.yh0720@gmail.com>
2021-09-14 19:24:57 +0900

    룸리스트 썸네일 추가

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -9,6 +9,7 @@ use App\Models\Room;
 use App\Models\RoomImage;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class RoomController extends Controller
 {
@@ -86,7 +87,9 @@ class RoomController extends Controller
         $s_no = $request->start_no;
         $row = $request->row;
 
-        $rows = Room::where('id','>=',$s_no)->orderBy('id', 'desc')->limit($row)->get();
+        $rows = Room::join('hotels', 'rooms.hotel_id', '=', 'hotels.id')
+                    ->select('*',DB::raw('(select file_name from room_images where room_images.room_id = rooms.id order by order_no asc limit 1 ) as thumb_nail'))
+                    ->where('rooms.id','>=',$s_no)->orderBy('rooms.id', 'desc')->limit($row)->get();
 
         $return = new \stdClass;
 
@@ -119,7 +122,7 @@ class RoomController extends Controller
     public function detail(Request $request){
         $id = $request->id;
 
-        $rows = Room::where('id','=',$id)->get();
+        $rows = Room::join('hotels', 'rooms.hotel_id', '=', 'hotels.id')->where('rooms.id','=',$id)->get();
         $images = RoomImage::where('room_id','=',$id)->orderBy('order_no')->get();
 
         $return = new \stdClass;
