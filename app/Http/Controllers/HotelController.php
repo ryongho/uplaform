f9ca6acc265f9a953a4fa6acda76d2d44b032ab6
yongho0720 <roh.yh0720@gmail.com>
2021-12-09 13:19:54 +0900

    헤더설정

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -117,7 +117,7 @@ class HotelController extends Controller
 
     public function list_by_partner(Request $request){
         
-        header("Access-Control-Allow-origin","*");
+        header("Access-Control-Allow-origin: *");
 
         $login_user = Auth::user();
 
