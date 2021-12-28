bb9315c9feae3b35fce63684d6b924f9ea881887
yongho0720 <roh.yh0720@gmail.com>
2021-12-28 13:04:57 +0900

    추천 리스트 출력값 변경

--- app/Http/Controllers/RecommendController.php
+++ app/Http/Controllers/RecommendController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Recommend;
 use App\Models\Goods;
+use App\Models\Hotel;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
 
@@ -47,10 +48,27 @@ class RecommendController extends Controller
 
 
         $rows = Recommend::join('goods', 'recommends.goods_id', '=', 'goods.id')
+                        ->join('rooms', 'goods.room_id', '=', 'rooms.id')
                         ->join('hotels', 'goods.hotel_id', '=', 'hotels.id')
-                        ->select('*',
-                            DB::raw('(select file_name from goods_images where goods_images.goods_id = recommends.goods_id order by order_no asc limit 1 ) as thumb_nail'),
-                        ) 
+                        ->select(   'hotels.type as shop_type', 
+                                    'rooms.name as room_name',
+                                    'hotels.name as hotel_name',
+                                    'goods.goods_name as goods_name', 
+                                    'goods.price as price',
+                                    'hotels.address as address',
+                                    'goods.sale_price as sale_price',
+                                    'goods.checkin as checkin',
+                                    'goods.checkout as checkout',
+                                    'goods.breakfast as breakfast',
+                                    'hotels.parking as parking',
+                                    'hotels.latitude as latitude',
+                                    'hotels.longtitude as longtitude',
+                                    'goods.id as goods_id',
+                                    'goods.sale as sale',
+                                    DB::raw('(select file_name from goods_images where goods_images.goods_id = goods.id order by order_no asc limit 1 ) as thumb_nail'),
+                                    DB::raw('(select avg(grade) from reviews where reviews.goods_id = goods.id) as grade'),
+                                    DB::raw('(select count(grade) from reviews where reviews.goods_id = goods.id) as grade_cnt'),
+                        )  
                         ->orderBy('order_no','asc')
                         ->get();
 
