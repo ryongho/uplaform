52c931033fb5edf52d57f9b1a496456f6cad6fee
yongho0720 <roh.yh0720@gmail.com>
2022-02-09 18:29:30 +0900

    출력 내용 수정

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -178,7 +178,7 @@ class ReservationController extends Controller
                 $app_info = Apply::where('reservation_id', $row['reservation_id'])->where('status', 'S')->first();
         
                 if($app_info != null){
-                    dd($app_info);
+                    dd($app_info['id']);
                     $user_info = PartnerInfo::where('user_id',$app_info['id'])->first();
                     $rows[$x]['matched_name'] = $user_info['ceo_name'];
                     $rows[$x]['phone'] = $user_info['tel'];
