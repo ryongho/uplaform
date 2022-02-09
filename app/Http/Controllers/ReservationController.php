3e3a3acf5f1369035b2d5a59c90e5e1459cbe3c6
yongho0720 <roh.yh0720@gmail.com>
2022-02-09 18:23:49 +0900

    출력 내용 수정

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -176,7 +176,7 @@ class ReservationController extends Controller
             foreach($rows as $row){
                 $app_info = Apply::where('reservation_id', $row['reservation_id'])->where('status', 'S')->first();
                 
-                if(isset($app_info)){
+                if(count($app_info) > 0){
                     $user_info = PartnerInfo::where('user_id',$app_info['id'])->first();
                     $rows[$x]['matched_name'] = $user_info['ceo_name'];
                     $rows[$x]['phone'] = $user_info['tel'];
