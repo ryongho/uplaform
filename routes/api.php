0f0e5c18782b4559942dc30950f50999bdde5d52
yongho0720 <roh.yh0720@gmail.com>
2022-02-13 15:28:37 +0900

    서비스 완료 기능 추가

--- routes/api.php
+++ routes/api.php
@@ -46,6 +46,7 @@ Route::middleware('auth:sanctum')->get('/admin/apply/list', [ApplyController::cl
 Route::middleware('auth:sanctum')->put('/admin/apply/match', [ApplyController::class, 'match']);
 Route::middleware('auth:sanctum')->put('/admin/reservation/update_service_address', [ReservationController::class, 'update_service_address']);
 Route::middleware('auth:sanctum')->put('/admin/reservation/cancel', [ReservationController::class, 'cancel_admin']);// 예약 취소
+Route::middleware('auth:sanctum')->put('/admin/reservation/complete', [ReservationController::class, 'complete']);// 서비스 완료
 
 Route::post('/user/regist/', [UserController::class, 'regist_user']); // 유저 등록
 Route::post('/partner/regist', [UserController::class, 'regist_partner']); // 파트너 등록 
