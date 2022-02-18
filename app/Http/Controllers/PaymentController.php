f1adec9987d1e70cbcfb5aa9549a962e93dcf55c
yongho0720 <roh.yh0720@gmail.com>
2022-02-18 18:00:33 +0900

    결제 디테일 추가

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -235,7 +235,7 @@ class PaymentController extends Controller
                             'reservations.canceled_at',
                             DB::raw('(select matched_at from applies where status = "S" and reservation_id = "reservations.id") as matched_at'),
                     )
-                    ->where('id',$payment_id)
+                    ->where('payments.id',$payment_id)
                     ->first();
 
 
