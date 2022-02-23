cbeeb22d618053b3d139a8bf8b0228eac7382549
yongho0720 <roh.yh0720@gmail.com>
2022-02-23 18:28:57 +0900

    출력값 수정

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -273,8 +273,8 @@ class ReservationController extends Controller
 
             }else if($row['reservation_type'] == "LC"){
                 $services = explode(',',$row['services']);
-                $service_infos = Service::whereIn('id',$row['services'])->distinct('service_sub_type')->get();
-                dd($service_infos);
+                $service_infos = Service::whereIn('id',$services)->distinct('service_sub_type')->get();
+                
                 foreach($service_infos as $service_info){
                     $rows[$x]['learn_type'] .= $service_info['service_sub_type'];
                 }
