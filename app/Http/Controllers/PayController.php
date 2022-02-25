f304e7453b8e0c8cb875daca0f9be1e9ff6d2069
yongho0720 <roh.yh0720@gmail.com>
2022-02-25 14:18:54 +0900

    cnt 수정

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -178,17 +178,16 @@ class PayController extends Controller
                     ->limit($row)
                     ->get();
 
-        /*$cnt = Pay::select(
+        $cnt = Pay::select(
                         DB::raw('DATE_FORMAT( pays.created_at, "%Y-%m" ) as month'),
                     )   
                     ->where('pays.created_at','>=',$start_month."-01 00:00:00")
                     ->where('pays.created_at','<=',$end_month."-31 23:59:59")
                     ->groupBy('month')
-                    
-                    ->count();*/
+                    ->get();
 
         $return->status = "200";
-        $return->cnt = count($rows);
+        $return->cnt = count($cnt);
         $return->data = $rows;
 
         return response()->json($return, 200)->withHeaders([
