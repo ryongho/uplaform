d988131bdae04938c4848ddaabf5defcb7664099
yongho0720 <roh.yh0720@gmail.com>
2022-02-25 13:54:59 +0900

    cnt 수정

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -179,12 +179,12 @@ class PayController extends Controller
                     ->get();
 
         $cnt = Pay::select(
-                    DB::raw('DATE_FORMAT( pays.created_at, "%Y-%m" ) as month'),
-                    DB::raw('count(*) as count'),
+                        DB::raw('DATE_FORMAT( pays.created_at, "%Y-%m" ) as month'),
                     )   
                     ->where('pays.created_at','>=',$start_month."-01 00:00:00")
                     ->where('pays.created_at','<=',$end_month."-31 23:59:59")
                     ->groupBy('month')
+                    ->orderby('month','desc')
                     ->count();
 
         $return->status = "200";
